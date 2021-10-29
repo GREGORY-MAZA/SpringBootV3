44ddfd1bec294a6aed2bbd8265b2a0e5d7654953
Greg <maza.gregory46@gmail.com>
2021-10-29 10:40:05 +0200

    Start V3 Spring Boot

---
+++ src/main/java/com/GregV3/SpringBootV3/SpringBootV3Application.java
@@ -0,0 +1,13 @@
+package com.GregV3.SpringBootV3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootV3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootV3Application.class, args);
+	}
+
+}
